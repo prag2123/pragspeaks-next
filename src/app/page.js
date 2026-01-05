ebfba3673f2757964b2140e625f2a435609cea58
Pragyanshu Labh <pragyanshu.labh@gmail.com>
2026-01-05 13:13:26 +1100

    Initial commit of Next.js project

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello, PragSpeaks is Coming Soon!</h1>
+      <p>This is my very first website built using Next.js!</p>
+    </main>
+  )
+}
